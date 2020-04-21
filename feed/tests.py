ba54d44177237b8f8f23fd39131903134faa3e19
Mohamed El Rahali <elrahali.md@gmail.com>
2020-04-21 04:38:33 +0000

    Feed Feature Tests - Complete

--- feed/tests.py
+++ feed/tests.py
@@ -63,6 +63,7 @@ class FeedTest(unittest.TestCase):
         code = user_1.change_configuration.get('confirmation_code')
         rv = self.app.get('/confirm/' + user_1.username + '/' + code)
         assert "Your email has been confirmed" in str(rv.data)
+
         """
             Login: User 1
         """
@@ -76,53 +77,201 @@ class FeedTest(unittest.TestCase):
             c.get('/')
             assert session.get('username') == user_1.username
 
-        # post a message on my own wall
-        self.app.post('/message/add', data=dict(
+        """
+             post a message on my own wall as User 1
+        """
+
+        response = self.app.post('/message/add', data=dict(
             to_user=user_1.username,
             post="Test First Post From User 1",
             images=[]
         ), follow_redirects=True)
 
-        # register user #2
+        assert "Test First Post From User 1" in str(response.data)
+
+        """
+            Registeration:  User 2
+        """
+        user_2 = self.getUser2()
+        self.app.post("/register", data=user_2, follow_redirects=True)
+        user_2 = User.getByName(username=user_2['username'])
+        code = user_2.change_configuration.get('confirmation_code')
+        rv = self.app.get('/confirm/' + user_2.username + '/' + code)
+        assert "Your email has been confirmed" in str(rv.data)
+
+        """
+            Make a Friendship Request to User 2
+        """
+        response = self.app.get('/add_friend/' + user_2.username, follow_redirects=True)
+        assert "Friendship Requested" in str(response.data)
+
+        """
+            Login as User 2
+            Confirm Friendship request from User 1
+        """
+
+        self.app.post('/login', data=dict(
+            username=user_2.username,
+            password=self.getUser2()['password']
+        ))
+
+        with self.app as c:
+            c.get('/')
+            assert session.get('username') == user_2.username
+
+        response = self.app.get('/add_friend/' + user_2.username, follow_redirects=True)
+        assert "Friends" in str(response.data)
+
+        """
+            Login: User 1
+        """
+
+        self.app.post('/login', data=dict(
+            username=user_1.username,
+            password=self.getUser1()['password']
+        ))
+
+        with self.app as c:
+            c.get('/')
+            assert session.get('username') == user_1.username
+
+        """
+            post a message on the feed
+        """
+
+        response = self.app.post('/message/add', data=dict(
+            to_user=user_1.username,
+            post="Test Post From User 1",
+            images=[]
+        ), follow_redirects=True)
+
+        assert "Test Post From User 1" in str(response.data)
+
+        """
+            write a message to User 2
+        """
+
+        response = self.app.post('/message/add', data=dict(
+            to_user=user_2.username,
+            post="Test Post From User 1 to User 2",
+            images=[]
+        ), follow_redirects=True)
+
+        assert "Test Post From User 1 to User 2" in str(response.data)
+
+        """
+            Login as User 2
+        """
+
+        self.app.post('/login', data=dict(
+            username=user_2.username,
+            password=self.getUser2()['password']
+        ))
+
+        with self.app as c:
+            c.get('/')
+            assert session.get('username') == user_2.username
+
+        """
+            Registeration:  User 3
+        """
+        user_3 = self.getUser3()
+        self.app.post("/register", data=user_3, follow_redirects=True)
+        user_3 = User.getByName(username=user_3['username'])
+        code = user_3.change_configuration.get('confirmation_code')
+        rv = self.app.get('/confirm/' + user_3.username + '/' + code)
+        assert "Your email has been confirmed" in str(rv.data)
 
 
-        # make friends with user #2
+        """
+            Make a Friendship Request to User 3
+        """
+        response = self.app.get('/add_friend/' + user_3.username, follow_redirects=True)
+        assert "Friendship Requested" in str(response.data)
 
+        """
+            Login as User 3
+            Confirm Friendship request from User 2
+        """
 
-        # login user #2 and confirm friend user #1
+        self.app.post('/login', data=dict(
+            username=user_3.username,
+            password=self.getUser3()['password']
+        ))
 
-        # login the first user again
+        with self.app as c:
+            c.get('/')
+            assert session.get('username') == user_3.username
 
+        response = self.app.get('/add_friend/' + user_3.username, follow_redirects=True)
+        assert "Friends" in str(response.data)
 
-        # post a message
+        """
+            Login as User 1
+        """
 
+        self.app.post('/login', data=dict(
+            username=user_1.username,
+            password=self.getUser1()['password']
+        ))
 
-        # post a message to user 2
+        with self.app as c:
+            c.get('/')
+            assert session.get('username') == user_1.username
 
+        """
+            Block User 3
+        """
+        response = self.app.get('/block/' + user_3.username, follow_redirects=True)
+        assert "Blocked" in str(response.data)
 
-        # login the second user
+        """
+            Login as User 2
+        """
 
-        # register user #3
+        self.app.post('/login', data=dict(
+            username=user_2.username,
+            password=self.getUser2()['password']
+        ))
 
+        with self.app as c:
+            c.get('/')
+            assert session.get('username') == user_2.username
 
-        # make friends with user #2
+        """
+            User 2 Confirm User 3
+        """
 
+        response = self.app.get('/add_friend/' + user_3.username, follow_redirects=True)
+        assert "Friends" in str(response.data)
 
-        # login the first user
+        """
+            write a message to User 3
+        """
 
+        response = self.app.post('/message/add', data=dict(
+            to_user=user_3.username,
+            post="Test Post From User 2 to User 3",
+            images=[]
+        ), follow_redirects=True)
 
-        # block user 3
+        assert "Test Post From User 2 to User 3" in str(response.data)
 
+        """
+            Login as User 1
+        """
 
-        # login the second user
+        self.app.post('/login', data=dict(
+            username=user_1.username,
+            password=self.getUser1()['password']
+        ))
 
-        # user 2 confirm friend user 3
+        with self.app as c:
+            c.get('/')
+            assert session.get('username') == user_1.username
 
-        # post a message to user 3
-
-
-        # login the first user
-
-
-        # check he doesn't see user 2's post to user 3 (blocked)
-
+        """
+            Check he doesn't see user 2's post to user 3 (blocked)
+        """
+        response = self.app.get('/')
+        assert "Test Post From User 2 to User 3" not in str(response.data)
